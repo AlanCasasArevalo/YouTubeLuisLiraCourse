20773f581734c3aeabef5c4473c896a422e91e2d
Alan Casas <alancasasarevalo@gmail.com>
2021-10-10 10:26:20 +0200

    * Games was returned

--- src/graphql/resolvers/character.ts
+++ src/graphql/resolvers/character.ts
@@ -1,9 +1,19 @@
 import {IResolvers} from '@graphql-tools/utils'
-import { characters } from '../../data/data.json'
+import { characters, games } from '../../data/data.json'
 const charactersResolver: IResolvers = {
     Query: {
         getCharacters() {
             return characters
+        }
+    },
+    Character: {
+        games(root: any, args: any, context: any, options: any) {
+            const gameList: Array<any> = []
+            root.games.map((gameId: string) => {
+                gameList.push(...games.filter(game => game._id === gameId))
+            })
+
+            return gameList
         }
     }
 }
